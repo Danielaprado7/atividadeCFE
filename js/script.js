efa75f21d48112778f0aa40ec1bee32c5119ce02
daniela <danielaprado7@gmail.com>
2023-02-20 13:43:56 -0300

    Home e login page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-// scrollTo - te leva para algum lugar da página
-function topo() {
+//efeito do botão voltar ao Topo
+function topo(){
     window.scrollTo({
         top: 0,
         left: 0,
@@ -7,23 +7,23 @@ function topo() {
     })
 }
 
-
-// Login
-function login() {
+//Validação de Login
+function login(){
     var logado = 0;
     var usuario = document.getElementsByName('usuario')[0].value;
     usuario = usuario.toLowerCase();
-    var senha = document.getElementsByName('senha')[0].value;
-    senha = senha.toLowerCase();
-    if (usuario == "admin" && senha == "123456") {
+    var senha = document.getElementById('senha').value;
+    senha = senha.toLowerCase();  
+    if(usuario == "admin" && senha == "123456") {
         window.location = "index.html";
         logado = 1;
     }
-    if (logado == 0) { alert("Acesso NEGADO. Dados incorretos, tente novamente!"); }
+    if(logado == 0){
+        alert ("Acesso negado. Dados Incorretos");
+    } 
 }
 
-
-// Cadastro
+//Ativar alert no botão cadastrar
 function cadastro() {
     alert("Cadastrado com sucesso!");
     window.location.href = "index.html";
